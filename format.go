dd82517092469dfa2f1d9bf927bb0454c3071dbc
解闯 <chuang.xie@enai.im>
2018-09-09 16:47:12 +0800

    format

--- format.go
+++ format.go
@@ -7,18 +7,18 @@ import (
 
 // FieldSnakeString input: XxYy output: xx_yy
 func FieldSnakeString(s string) string {
-	data := make([]byte, 0, len(s)*2)
-	j := false
+	res := make([]byte, 0, len(s)*2)
+	flag := false
 	for _, d := range StringToBytes(s) {
 		if d >= 'A' && d <= 'Z' {
-			if j {
-				data = append(data, '_')
-				j = false
+			if flag {
+				res = append(res, '_')
+				flag = false
 			}
 		} else if d != '_' {
-			j = true
+			flag = true
 		}
-		data = append(data, d)
+		res = append(res, d)
 	}
-	return strings.ToLower(*(*string)(unsafe.Pointer(&data)))
+	return strings.ToLower(*(*string)(unsafe.Pointer(&res)))
 }
