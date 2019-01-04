18f5d53c9664e8cebc3a8496acba9558463eaa28
swxctx <swxctxone@yeah.net>
2019-01-04 17:51:50 +0800

    随机串生成

---
+++ rand.go
@@ -0,0 +1,18 @@
+package gutil
+
+import (
+	"crypto/rand"
+	"time"
+)
+
+// 生成随机字符串
+func GetRandomString(length int) string {
+	str := "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	bytes := []byte(str)
+	result := []byte{}
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for i := 0; i < length; i++ {
+		result = append(result, bytes[r.Intn(len(bytes))])
+	}
+	return string(result)
+}
