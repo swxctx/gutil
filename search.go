a34211cce569f4a6070702d33dec4b81cd102d53
swxctx <swxctxone@yeah.net>
2019-02-22 17:36:42 +0800

    elem in slice

--- search.go
+++ search.go
@@ -62,3 +62,53 @@ func Int64BinarySearch(array []int64, key int64) int {
 	}
 	return -1
 }
+
+// Int32InSlice 判断int32是否在切片里面
+func Int32InSlice(needle int32, haystack []int32) bool {
+	for _, b := range haystack {
+		if b == needle {
+			return true
+		}
+	}
+	return false
+}
+
+// StringInSlice 判断字符是否在切片里面
+func StringInSlice(needle string, haystack []string) bool {
+	for _, b := range haystack {
+		if b == needle {
+			return true
+		}
+	}
+	return false
+}
+
+// Int64InSlice 判断int64是否在切片里面
+func Int64InSlice(needle int64, haystack []int64) bool {
+	for _, b := range haystack {
+		if b == needle {
+			return true
+		}
+	}
+	return false
+}
+
+// IntInSlice 判断int是否在切片里面
+func IntInSlice(needle int, haystack []int) bool {
+	for _, b := range haystack {
+		if b == needle {
+			return true
+		}
+	}
+	return false
+}
+
+// InterfaceInSlice 判断interface是否在切片里面
+func InterfaceInSlice(needle interface{}, haystack []interface{}) bool {
+	for _, b := range haystack {
+		if b == needle {
+			return true
+		}
+	}
+	return false
+}
