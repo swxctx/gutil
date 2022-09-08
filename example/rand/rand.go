6bac54ec8d729b4df3ced25a750ebd83bedd5d23
swxctx <swxctx@gmail.com>
2022-09-08 19:06:12 +0800

    add rand example

---
+++ example/rand/rand.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"github.com/swxctx/gutil"
+)
+
+func main() {
+	fmt.Println(gutil.NewRandom(gutil.RandLowerWordsAndNumber).RandomString(10))
+	fmt.Println(gutil.NewRandom(gutil.RandUpperWordsAndNumber).RandomString(10))
+	fmt.Println(gutil.NewRandom(gutil.RandLowerWords).RandomString(10))
+	fmt.Println(gutil.NewRandom(gutil.RandUpperWords).RandomString(10))
+	fmt.Println(gutil.NewRandom(gutil.RandLowerAndUpperWords).RandomString(10))
+	fmt.Println(gutil.NewRandom(gutil.RandLowerAndUpperWordsAndNumber).RandomString(10))
+	fmt.Println(gutil.NewRandom(gutil.RandNumbers).RandomString(10))
+}
