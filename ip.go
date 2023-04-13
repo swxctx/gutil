f458bb07974a9b4f4345a9692c280927a2978c55
swxctx <swxctx@gmail.com>
2023-04-13 16:35:11 +0800

    add get host local inner ip

--- ip.go
+++ ip.go
@@ -73,3 +73,20 @@ func ParseIpToUint32(s string) (uint32, error) {
 	ip |= uint32(ipObj[15])
 	return ip, nil
 }
+
+// GetLocalInnerIP get host local inner ip
+func GetLocalInnerIP() (string, error) {
+	address, err := net.InterfaceAddrs()
+	if err != nil {
+		return "", err
+	}
+	for _, address := range address {
+		// 检查ip地址判断是否回环地址
+		if ipNet, ok := address.(*net.IPNet); ok && !ipNet.IP.IsLoopback() {
+			if ipNet.IP.To4() != nil {
+				return ipNet.IP.String(), nil
+			}
+		}
+	}
+	return "", nil
+}
